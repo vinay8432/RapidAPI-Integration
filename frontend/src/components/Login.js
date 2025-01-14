cc67f98b704b5e0d622281ccb0c5519d148cf508
vinay singhal <vinay.singhal@netenrich.com>
2025-01-14 19:44:53 +0530

    added cred

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -10,7 +10,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/login', { email, password });
+      const response = await axios.post('http://rapid-api-integration-backend.vercel.app/api/auth/login', { email, password });
       localStorage.setItem('token', response.data.token); // Store token
       navigate('/portfolio'); // Redirect to portfolio
     } catch (error) {
